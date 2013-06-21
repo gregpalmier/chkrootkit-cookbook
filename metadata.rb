6e8076e1e50c3ff88cfe3b9fa8044a6dfd2a4632
Greg Palmier <gregpalmier@gmail.com>
2013-06-21 10:46:39 -0500

    fix license metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'chkrootkit'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'gregpalmier'
+maintainer_email 'gregpalmier@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures chkrootkit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
